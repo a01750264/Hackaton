1f3a6b4378d85fb697565fafd2a88fdb315adcad
a01750264 <a01750246@itesm.mx>
2021-10-24 02:05:00 -0500

    cambios

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
+const puerto = 8080;
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/dashboard', (req, res)=>{
+    res.sendFile(path.join(__dirname, '.', 'views', 'index.html'))
+});
+
+app.get('/queLesInteresa', (req, res)=>{
+    res.sendFile(path.join(__dirname, '.', 'views', 'queLesInteresa.html'))
+})
+
+app.get('/extra', (req, res)=>{
+    res.sendFile(path.join(__dirname, '.', 'views', 'extra.html'))
+})
+
+app.listen(puerto, ()=>{
+    console.log(`Servidor en línea en el puerto ${puerto}`)
+});
